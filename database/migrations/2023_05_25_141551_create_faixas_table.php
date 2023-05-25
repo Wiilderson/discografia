f153c9254cb0991c3bcd1b58a7354a6128661b3c
wilderson <wilderson@uitecnologia.com.br>
2023-05-25 12:03:24 -0300

    feat: migrations db

---
+++ database/migrations/2023_05_25_141551_create_faixas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faixas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('album_id');
+            $table->unsignedBigInteger('numero_faixa');
+            $table->string('faixas');
+            $table->time('duracao');
+            $table->timestamps();
+            $table->foreign('album_id')->references('id')->on('albums')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faixas');
+    }
+};
